4dc8e46c03375aa7ea949c2122538fc92499992c
Steppenbar <hiwattslo@gmail.com>
2020-02-28 21:30:49 -0800

    definitely added buttons

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,9 @@
 function generateMarkdown(data) {
-
-  return `
+  return `# ${data.title}
+  [![ForTheBadge uses-js](http://ForTheBadge.com/images/badges/uses-js.svg)](http://ForTheBadge.com)
+  [![ForTheBadge built-with-science](http://ForTheBadge.com/images/badges/built-with-science.svg)](https://GitHub.com/Naereen/)]
+  [![forthebadge made-with-python](http://ForTheBadge.com/images/badges/made-with-python.svg)](https://www.python.org/)
+ 
     # ${data.Username}
     ${data["number of repos"]}
     ${data["User location"]}
