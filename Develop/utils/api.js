f5a4e78ef330e6681fbbe1977beafc12d1736ad0
Steppenbar <hiwattslo@gmail.com>
2020-02-28 18:48:56 -0800

    added code for questions and API

---
+++ Develop/utils/api.js
@@ -0,0 +1,25 @@
+const api = {
+  getUser(username) {
+
+  }
+};
+
+module.exports = api;
+
+
+
+// const axios = require("axios");
+
+// axios
+//   .get("https://www.omdbapi.com/?t=The%20Matrix&apikey=trilogy")
+//   .then(function(res) {
+//     console.log(res.data);
+//   });
+
+
+
+
+// .then(({username}) => { //query Github API for user info and repos
+//   const repoQueryUrl = `https://api.github.com/users/${username}/repos`;
+//   const accQueryUrl = `https://api.github.com/users/${username}`;
+//   console.log("API queried");
