e0dfd342fe20f754a1ea80362614356356681051
Steppenbar <hiwattslo@gmail.com>
2020-02-29 10:06:39 -0800

    testing buttons

--- Develop/ReadMe.md
+++ Develop/ReadMe.md
@@ -1,4 +1,19 @@
+# fox
+  [![ForTheBadge uses-js](http://ForTheBadge.com/images/badges/uses-js.svg)](http://ForTheBadge.com)
+  [![ForTheBadge built-with-science](http://ForTheBadge.com/images/badges/built-with-science.svg)](https://GitHub.com/Naereen/)]
+  [![forthebadge made-with-python](http://ForTheBadge.com/images/badges/made-with-python.svg)](https://www.python.org/)
+  
+  ## Description
+  * its really nice
+  ![Questions](utils/images/example.gif)]
+   ## Table of Contents
+  - [Installation](#Installation)
+  - [Usage](#Usage)
+  - [License](#License)
+  - [Contributing](#Contributing)
+  - [Test](#Test)
+  - [Questions](#Questions)
 
     # steppenbar
-    16
-    San Diego
+    undefined
+    undefined
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -3,7 +3,18 @@ function generateMarkdown(data) {
   [![ForTheBadge uses-js](http://ForTheBadge.com/images/badges/uses-js.svg)](http://ForTheBadge.com)
   [![ForTheBadge built-with-science](http://ForTheBadge.com/images/badges/built-with-science.svg)](https://GitHub.com/Naereen/)]
   [![forthebadge made-with-python](http://ForTheBadge.com/images/badges/made-with-python.svg)](https://www.python.org/)
- 
+  
+  ## Description
+  * ${data.description}
+  ![Questions](utils/images/example.gif)]
+   ## Table of Contents
+  - [Installation](#Installation)
+  - [Usage](#Usage)
+  - [License](#License)
+  - [Contributing](#Contributing)
+  - [Test](#Test)
+  - [Questions](#Questions)
+
     # ${data.Username}
     ${data["number of repos"]}
     ${data["User location"]}
